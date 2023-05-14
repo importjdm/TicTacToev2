5079de792d349f3df734b7e81ec7c02166d97b70
Emmanuel Guerrero <emanuel.gue@gmail.com>
2023-05-13 19:50:48 -0500

    set board still completeing rest of functions

--- tttV2.js
+++ tttV2.js
@@ -1,1 +1,71 @@
 //check check
+//functions (board, controling, players, displaying)
+
+const gameBoard = () => {
+  let board = ["x", "x", "x", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  return { getBoard };
+};
+
+const displaying = () => {
+  const domBoard = document.querySelector(".board");
+  const displayText = document.querySelector(".displayHeadings");
+  const board = gameBoard();
+  const playingBoard = board.getBoard();
+  const displayTextNShit = () => {};
+
+  const displayBoard = () => {
+    for (emtypCell of playingBoard) {
+      let button = document.createElement("button");
+      button.classList.add("cell");
+      domBoard.appendChild(button);
+    }
+  };
+
+  return { displayBoard };
+};
+
+const display = displaying();
+display.displayBoard();
+
+const players = () => {
+  let playerOne = {
+    name: "",
+    marker: "X",
+  };
+  let playerTwo = {
+    name: "",
+    marker: "O",
+  };
+
+  let currentPLayer = playerOne;
+
+  const getCurrentPlayer = () => currentPLayer;
+
+};
+
+const boardControl = () => {
+    const displayGrid = displaying();
+    const board. 
+  const reset = () => {};
+
+  const checkForWinner = () => {};
+
+  const setMaker = () => {
+    currentPLayer.marker 
+  };
+
+  const switchTurn = () => {};
+
+  const addListener = () => {
+    
+    const listenerGrid = displayGrid.displayBoard();
+    listenerGrid.addListener("click",setMarker )
+
+  }
+
+  
+};
+//figuring out how to set the marker in the correct spot
