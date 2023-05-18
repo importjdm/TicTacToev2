6893e623a355303f20e0de5c714a0fb22ffe2442
Emmanuel Guerrero <emanuel.gue@gmail.com>
2023-05-17 21:45:43 -0500

    progress on check for winner function

--- tttV2.js
+++ tttV2.js
@@ -71,36 +71,78 @@ const boardControl = () => {
   };
 
   const checkForWinner = () => {
+    let checkForWinnerArray = [];
+    let markerArray = Array();
+
     let leftToRightTop = document.querySelectorAll(
       "[data-id='0'],[data-id='1'],[data-id='2']"
     );
-    const leftToRightMid = document.querySelector(
-      "[data-id='0'][data-id='1'][data-id='2']"
+    let leftToRightMid = document.querySelectorAll(
+      "[data-id='3'],[data-id='4'],[data-id='5']"
     );
-    const leftToRightLow = document.querySelector(
-      "[data-id='0'][data-id='1'][data-id='2']"
+    let leftToRightLow = document.querySelectorAll(
+      "[data-id='6'],[data-id='7'],[data-id='8']"
     );
-    const upDownLeft = document.querySelector(
-      "[data-id='0'][data-id='3'][data-id='6']"
+    let upDownLeft = document.querySelectorAll(
+      "[data-id='0'],[data-id='3'],[data-id='6']"
     );
-    const upDownMid = document.querySelector(
-      "[data-id='1'][data-id='4'][data-id='7']"
+    let upDownMid = document.querySelectorAll(
+      "[data-id='1'],[data-id='4'],[data-id='7']"
     );
-    const upDownRight = document.querySelector(
-      "[data-id='2'][data-id='5'][data-id='8']"
+    let upDownRight = document.querySelectorAll(
+      "[data-id='2'],[data-id='5'],[data-id='8']"
     );
-    const diaLeftToRight = document.querySelector(
-      "[data-id='0'][data-id='4'][data-id='8']"
+    let diaLeftToRight = document.querySelectorAll(
+      "[data-id='0'],[data-id='4'],[data-id='8']"
     );
-    const diaRightToLeft = document.querySelector(
-      "[data-id='2'][data-id='4'][data-id='6']"
+    let diaRightToLeft = document.querySelectorAll(
+      "[data-id='2'],[data-id='4'],[data-id='6']"
     );
 
-    /* leftToRightTop.forEach((button) => {
-        if(button.textContent === )
-    }); */
+    /*console.log(leftToRightTop);
+    console.log(leftToRightMid);*/
+    checkForWinnerArray.push(
+      leftToRightTop,
+      leftToRightMid,
+      leftToRightLow,
+      upDownLeft,
+      upDownMid,
+      upDownRight,
+      diaLeftToRight,
+      diaRightToLeft
+    );
+    console.log(checkForWinnerArray);
+    
+    //get eveything into array slice every 3 indexes represents a winner. 
+    //9 arrays of possible ways to win if any has all 3 of the currentplayr marker
+    //they win
+    
+    checkForWinnerArray.forEach((nList) => {
+      nList.forEach((button) => {
+        markerArray.push(button.innerText);
+      });
+      for(let i = 2; i < markerArray.length; i + 3){
+        let array + i = arkerArray.splice(0, i)
+      }
+      console.log(markerArray);
+    });
 
-    console.log(leftToRightTop.textContent);
+    
+    //puts the nodeist in a array
+    /* console.log(leftToRightTop);
+    Array.from(leftToRightTop, (buttonText) => {
+      checkForWinnerArray.push(buttonText.innerText);
+    });
+    
+    //check array for three in a row 
+    const letsCheckArray = () =>{
+        for(let i = 0; i < 2; i++){
+            if(checkForWinnerArray[i] === player.getCurrentPlayer().marker){
+
+            }
+            console.log(player.getCurrentPlayer().name + "is the winner");
+        }
+    } */
   };
 
   const setMarker = (x) => {
@@ -123,7 +165,6 @@ const boardControl = () => {
 
   const playRound = () => {
     boardElement.addEventListener("click", (e) => {
-      console.log(e);
       let startChecking = 1;
       let markerLocation = getDataID(e);
       setMarker(markerLocation);
